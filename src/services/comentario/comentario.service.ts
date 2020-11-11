f379fea01878af7648932f9c77c46c7961f1e147
cedececa <cedececa@gmail.com>
2020-11-11 02:35:05 +0100

    add comentario service

---
+++ src/services/comentario/comentario.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { ComentarioEntity } from 'src/entities/Comentario.entity'
+import { Repository } from 'typeorm'
+import { CommonService } from '../common.service'
+
+@Injectable()
+export class ComentarioService extends CommonService<ComentarioEntity> {
+  constructor(
+    @InjectRepository(ComentarioEntity)
+    repo: Repository<ComentarioEntity>,
+  ) {
+    super(repo, 'comentario')
+  }
+}
