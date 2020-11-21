5954cdf1eb56345fd809c67e1b6e4102eae89598
cedececa <cedececa@gmail.com>
2020-11-21 13:14:28 +0100

    add service REST for publicacion and comentario

--- src/services/comentario/comentario.service.ts
+++ src/services/comentario/comentario.service.ts
@@ -12,4 +12,25 @@ export class ComentarioService extends CommonService<ComentarioEntity> {
   ) {
     super(repo, 'comentario')
   }
+
+  async getManyByPublicacionId(publicacionId: number) {
+    const queryBuilder = this.repo.createQueryBuilder('comentario')
+    const comentarios = await queryBuilder
+      .leftJoin('comentario.publicacion', 'publicacion')
+      .leftJoin('comentario.usuario', 'usuario')
+      .leftJoin('usuario.perfil', 'perfil')
+      .where('publicacion.id = :publicacionId', {
+        publicacionId,
+      })
+      //seleccionamos los datos que queremos
+      .select([
+        'publicacion.id',
+        'comentario.contenido',
+        'comentario.createdAt',
+        'usuario.id',
+        'perfil.nombre',
+      ])
+      .getMany()
+    return comentarios
+  }
 }
