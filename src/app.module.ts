e105427fa816b77cf0568300c6dd807c543081df
cedececa <cedececa@gmail.com>
2020-11-21 16:41:25 +0100

    add external services for datos abiertos (ropa y pilas)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common'
+import { HttpModule, Module } from '@nestjs/common'
 import { ComentarioModule } from './features/comentario/comentario.module'
 import { UsuarioModule } from './features/usuario/usuario.module'
 import { FotoModule } from './features/foto/foto.module'
 import { PerfilModule } from './features/perfil/perfil.module'
 import { PublicacionModule } from './features/publicacion/publicacion.module'
 import { ValoracionModule } from './features/valoracion/valoracion.module'
+import { ExternalServicesModule } from './features/external-services/external-services.module'
 import DatabaseORM from './database'
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import DatabaseORM from './database'
     PublicacionModule,
     ValoracionModule,
     ComentarioModule,
+    ExternalServicesModule,
   ],
 })
 export class AppModule {}
