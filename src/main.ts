f769e97cd27f385c1a62f6d1ec409fdd3e4f0241
cedececa <cedececa@gmail.com>
2020-11-22 19:07:34 +0100

    add swagger generator.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as helmet from 'helmet'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
+async function APIsDocument(app) {
+  const options = new DocumentBuilder()
+    .setTitle('Grafitis APIs')
+    .setDescription('Grafitis API description')
+    .setVersion('1.0')
+    .addTag('Grafitis')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-
+  APIsDocument(app)
   // enable CORS
   app.use(helmet())
   app.enableCors()
